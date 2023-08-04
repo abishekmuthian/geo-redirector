62b7c7cdba9bf5cd5c2b0cd8162c66b0f1c3918c
abishek <heavyinfo@gmail.com>
2023-08-04 16:37:15 +0530

    Analytics server code, added params

--- src/routes/(product)/product/[name]/[id]/analytics/+page.server.ts
+++ src/routes/(product)/product/[name]/[id]/analytics/+page.server.ts
@@ -1,15 +1,17 @@
 import type { PageServerLoad } from "../$types";
 
-export const load: PageServerLoad = async ({ locals }) => {
-  //   if (locals.user) {
+export const load: PageServerLoad = async ({ locals, params }) => {
+  if (locals.user) {
+    const analyticsData = new Map();
 
-  const analyticsData = new Map();
-  analyticsData.set("Sri Lanka", 10);
-  analyticsData.set("India", 20);
-  analyticsData.set("Bhutan", 5);
+    // Fetch product with name
+    // Iterate Analytics and set the country & views
+    analyticsData.set("Sri Lanka", 10);
+    analyticsData.set("India", 20);
+    analyticsData.set("Bhutan", 5);
 
-  return {
-    analyticsData: analyticsData,
-  };
-  //   }
+    return {
+      analyticsData: analyticsData,
+    };
+  }
 };
